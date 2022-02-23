19b3bc07753496b06d61d0fba0769d2b55fc386c
malcops <paul.malcolm@protonmail.com>
2022-02-22 19:06:38 -0500

    Add initial MPU6050 files

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "MPU6050.hpp"
+
+int main(void){
+
+    MPU6050 imu;
+    imu.init();
+    while(1){
+
+        auto accels = imu.readAccelXYZ(); 
+        char accelOutput[128];
+        snprintf(accelOutput, sizeof(accelOutput), "accels %f %f %f\n", accels.accel_X, accels.accel_Y, accels.accel_Z);
+        std::cout << accelOutput << std::endl;
+
+        auto gyros = imu.readGyroXYZ();
+        char gyroOutput[50];
+        snprintf(gyroOutput, sizeof(gyroOutput), "gyros %f %f %f\n", gyros.gyro_X, gyros.gyro_Y, gyros.gyro_Z);
+        std::cout << gyroOutput << std::endl;
+
+        std::cout << imu.readTemp() << std::endl;
+        sleep(1);
+    }
+
+    return 0;
+}
---
+++ test_MPU6050.cpp
@@ -0,0 +1,95 @@
+#include "MPU6050.hpp"
+#include "gtest/gtest.h"
+
+TEST(accelConversion, 0000) {
+  EXPECT_NEAR(accelConversion(0x0000), 0.0, 0.05);
+}
+
+TEST(accelConversion, be28) {
+  EXPECT_NEAR(accelConversion(0xbe28), -1.0, 0.05);
+}
+
+TEST(accelConversion, bd0c) {
+  EXPECT_NEAR(accelConversion(0xbd0c), -1.0, 0.05);
+}
+
+TEST(accelConversion, 3ff8) {
+  EXPECT_NEAR(accelConversion(0x3ff8), 1.0, 0.05);
+}
+
+TEST(accelConversion, 4074) {
+  EXPECT_NEAR(accelConversion(0x4074), 1.0, 0.05);
+}
+
+// almost 2g -> maximum measurement
+TEST(accelConversion, 7fff) {
+  EXPECT_NEAR(accelConversion(0x7fff), 2.0, 0.05);
+}
+
+// -2g -> maximum measurement
+TEST(accelConversion, 8000) {
+  EXPECT_NEAR(accelConversion(0x8000), -2.0, 0.05);
+}
+
+TEST(accelConversion, 8001) {
+  EXPECT_NEAR(accelConversion(0x8001), -2.0, 0.05);
+}
+
+TEST(accelConversion, ffff) {
+  EXPECT_NEAR(accelConversion(0xffff), -0.0, 0.05);
+}
+
+TEST(gyroConversion, 0000){
+  EXPECT_NEAR(gyroConversion(0x0000), 0.0, 0.25);
+}
+
+TEST(gyroConversion, 4000){
+  EXPECT_NEAR(gyroConversion(0x4000), 125.0, 0.25);
+}
+
+TEST(gyroConversion, 7fff){
+  EXPECT_NEAR(gyroConversion(0x7fff), 250.0, 0.25);
+}
+
+TEST(gyroConversion, 8000){
+  EXPECT_NEAR(gyroConversion(0x8000), -250.0, 0.25);
+}
+
+TEST(gyroConversion, 8001){
+  EXPECT_NEAR(gyroConversion(0x8001), -250.0, 0.25);
+}
+
+TEST(gyroConversion, ffff){
+  EXPECT_NEAR(gyroConversion(0xffff), -0.0, 0.25);
+}
+
+// TODO
+TEST(tempConversion, 0000){
+  EXPECT_NEAR(tempConversion(0x0000), 36.53, 0.1);
+}
+
+TEST(tempConversion, 4000){
+  EXPECT_NEAR(tempConversion(0x4000), 84.7, 0.1);
+}
+
+TEST(tempConversion, 7fff){
+  EXPECT_NEAR(tempConversion(0x7fff), 132.9, 0.1);
+}
+
+TEST(tempConversion, 8000){
+  EXPECT_NEAR(tempConversion(0x8000), -59.8, 0.1);
+}
+
+TEST(tempConversion, b000){
+  EXPECT_NEAR(tempConversion(0xb000), -23.7, 0.1);
+}
+
+TEST(tempConversion, ffff){
+  EXPECT_NEAR(tempConversion(0xffff), 36.5, 0.1);
+}
+
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
